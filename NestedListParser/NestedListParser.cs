9a9761726c91f30cd4a9d18c64c75f08eebe761d
Ryan Lerch <lerchr@gmail.com>
2016-09-22 20:00:47 -0400

    Working

---
+++ NestedListParser/NestedListParser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NestedListParser
+{
+    public class ParseResult
+    {
+        public string Value { get; set; }
+        public int Level { get; set; }
+
+        public override string ToString()
+        {
+            var listOfHyphens = Enumerable.Range(0, Level).Select((x) => "-");
+            return $"{string.Join(string.Empty, listOfHyphens)} {Value}";
+        }
+    }
+
+    public class NestedListParser
+    {
+        private readonly Regex _openParen = new Regex("^\\(", RegexOptions.None);
+        private readonly Regex _wordRegex = new Regex(@"^(,?\s*)(?<value>\w+)(,?)", RegexOptions.None);
+        private readonly Regex _closeParen = new Regex("^\\)", RegexOptions.None);
+
+        private bool Matches(Regex regex, string toCheck)
+        {
+            var matches = regex.Match(toCheck);
+            return matches.Success;
+        }
+
+        private string GetValue(string toCheck)
+        {
+            var match = _wordRegex.Match(toCheck);
+            return match.Groups["value"].Value;
+        }
+
+        public List<ParseResult> Parse(string toParse, int level = -1)
+        {
+            var toReturn = new List<ParseResult>();
+
+            if (Matches(_openParen, toParse))
+            {
+                var trimmed = _openParen.Replace(toParse, string.Empty);
+                toReturn.AddRange(Parse(trimmed, level + 1));
+            }
+
+            if (Matches(_wordRegex, toParse))
+            {
+                var value = GetValue(toParse);
+                var trimmed = _wordRegex.Replace(toParse, string.Empty);
+                toReturn.Add(new ParseResult {
+                    Value = value,
+                    Level = level
+                });
+                toReturn.AddRange(Parse(trimmed, level));
+            }
+
+            if (Matches(_closeParen, toParse))
+            {
+                var trimmed = _closeParen.Replace(toParse, string.Empty);
+                toReturn.AddRange(Parse(trimmed, level - 1));
+            }
+
+
+            return toReturn;
+        } 
+    }
+}
---
+++ NestedListParser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedListParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var toParse = "(id,created,employee(id,firstname,employeeType(id), lastname),location)";
+            var parser = new NestedListParser();
+            var parseResults = parser.Parse(toParse);
+
+            foreach (var parseResult in parseResults.OrderBy(x => x.Value).ThenBy(x => x.Level))
+            {
+                Console.WriteLine(parseResult);
+            }
+            //Keep window open
+            Console.Read();
+        }
+    }
+}
