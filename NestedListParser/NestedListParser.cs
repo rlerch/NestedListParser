12ba0ce090ba92e8818dd947c27a8abf2926b7e1
Ryan Lerch <ryan.lerch@assurancesoftware.com>
2019-03-05 09:13:21 -0500

    Remove unused list and change comment

--- NestedListParser/NestedListParser.cs
+++ NestedListParser/NestedListParser.cs
@@ -37,7 +37,7 @@ namespace NestedListParser
 
     /// <summary>
     /// The idea of this class is to chomp away the string bit by bit from the front and
-    /// generate a list of ParseResults as we encounter keywords, keeping track of
+    /// generate a tree of ParseResults as we encounter keywords, keeping track of
     /// the nesting level we are currently at. Inspired by a recursive decent parser.
     /// </summary>
     public class NestedListParser
@@ -64,33 +64,32 @@ namespace NestedListParser
                 return null;
 
             parent = parent ?? new ParseResult { Value = "root" }; //Root node so that every real node has a parent and the tree has a single top node
-            var toReturn = new List<ParseResult>();
 
             if (Matches(_openParen, toParse))
             {
-                HandleOpenParen(toParse, level, toReturn, parent);
+                HandleOpenParen(toParse, level, parent);
             }
 
             if (Matches(_wordRegex, toParse))
             {
-                HandleKeyword(toParse, level, toReturn, parent);                
+                HandleKeyword(toParse, level, parent);                
             }
 
             if (Matches(_closeParen, toParse))
             {
-                HandleCloseParen(toParse, level, toReturn, parent);
+                HandleCloseParen(toParse, level, parent);
             }
 
             return parent;
         }
 
-        private void HandleCloseParen(string toParse, int level, List<ParseResult> toReturn, ParseResult parent)
+        private void HandleCloseParen(string toParse, int level, ParseResult parent)
         {
             var trimmed = _closeParen.Replace(toParse, string.Empty);
             Parse(trimmed, level - 1, parent.Parent);
         }
 
-        private void HandleKeyword(string toParse, int level, List<ParseResult> toReturn, ParseResult parent)
+        private void HandleKeyword(string toParse, int level, ParseResult parent)
         {
             var value = GetValue(toParse);
             var trimmed = _wordRegex.Replace(toParse, string.Empty);
@@ -106,7 +105,7 @@ namespace NestedListParser
             Parse(trimmed, level, parent);
         }
 
-        private void HandleOpenParen(string toParse, int level, List<ParseResult> toReturn, ParseResult parent)
+        private void HandleOpenParen(string toParse, int level, ParseResult parent)
         {
             var trimmed = _openParen.Replace(toParse, string.Empty);
             Parse(trimmed, level + 1, parent?.Children?.LastOrDefault() ?? parent);
